07f66d3763903b8d91c83f58ca62fd1ed8f266de
Your Name <your.email@example.comgit config __global user.name Paitynleigh>
2025-11-21 13:32:11 -0800

    changes

--- Flex.java
+++ Flex.java
@@ -1,2 +1,23 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+
+
+public class Flex {
+    String file = "Data/Stored_Value_Transaction_by_Customer__11_39_2025-10-17_11_40_52(Stored_Value_Transaction_by_Cus).csv";
+    String line;
+
+    DateTimeFormatter dater = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+
+    try(BufferedReader br = new BufferedReader(new FileReader(file))){
+        br.readLine();
+        while((line = br.readline()) != null){
+
+        }
+    }
+}
 
