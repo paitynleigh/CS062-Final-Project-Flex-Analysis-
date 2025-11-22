e191c2e78a84ff43b4ef814f47f9c702407134a0
Your Name <your.email@example.comgit config __global user.name Paitynleigh>
2025-11-21 21:11:24 -0800

    test class comments

--- TestFlex.java
+++ TestFlex.java
@@ -1,18 +1,18 @@
 import java.util.List;
 import java.util.Map;
 
-public class TestFlex {
-    public static void main(String[] args){
-        Flex testFlex = new Flex();
+//public class TestFlex {
+    //public static void main(String[] args){
+        //Flex testFlex = new Flex();
 
-        testFlex.loadCSV("Data/Stored_Value_Transaction_by_Customer__11_39_2025-10-17_11_40_52(Stored_Value_Transaction_by_Cus).csv");
+       // testFlex.loadCSV("Data/Stored_Value_Transaction_by_Customer__11_39_2025-10-17_11_40_52(Stored_Value_Transaction_by_Cus).csvnull");
 
-        Map<String, Map<String, List<Transaction>>> data = testFlex.getData();
+      //  Map<String, Map<String, List<Transaction>>> data = testFlex.getData();
 
         // Example: print all transactions at a location and school
-        List<Transaction> list = data.get("Coop Store").get("POM");
-        for(Transaction t : list){
-            System.out.println(t);  // uses Transaction.toString()
-        }
-    }
-}   
+    //    List<Transaction> list = data.get("Coop Store").get("POM");
+    //  for(Transaction t : list){
+      //      System.out.println(t);  // uses Transaction.toString()
+   //     }
+ //   }
+//}   
