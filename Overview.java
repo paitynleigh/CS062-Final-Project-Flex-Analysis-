f503fb2712219eca0c6a93e9257cb997897a42ca
Your Name <your.email@example.comgit config __global user.name Paitynleigh>
2025-12-11 08:18:15 -0600

    overview

---
+++ Overview.java
@@ -0,0 +1,32 @@
+public class Overview {
+    
+    // This method prints out each location and the LEST busy week day at that location 
+    public String printLeastBusy(){
+        //to-do
+        return "";
+    }
+
+    // This method prints out each location and the MOST busy week day at that location 
+    public String printMostBusy(){
+        //to-do
+        return "";
+    }
+
+    // This method prints out the average dollar amount per transaction for each school
+    public String printTransactionAmount(){
+        //to-do
+        return "";
+    }
+
+    // This method prints out the TOTAL dollar amount spent over the semester per school
+    public String printTotalAmount(){
+        //to-do
+        return "";
+    }
+
+    // This method prints the total number of transactions per each location
+    public String printNumTransactions(){
+        //to-do
+        return "";
+    }
+}
