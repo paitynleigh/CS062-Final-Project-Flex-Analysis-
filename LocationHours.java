8b378cba139843bd05e489a9960c99ef0a9ae412
NateW16 <njwf2024@mymail.pomona.edu>
2025-12-10 19:33:54 -0800

    LocationHours updates

--- LocationHours.java
+++ LocationHours.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 
 public class LocationHours {
@@ -6,13 +6,34 @@ public class LocationHours {
 
 
     public LocationHours(File locationHourData){
-        // while there are more locations in the file add them to the hashmap
-        while (true) { 
-            
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(locationHourData));
+            String singleLocationData = br.readLine();
+            // while there are more locations in the file add them to the hashmap
+            while (singleLocationData != null) { 
+                addLocationHours(singleLocationData);
+            }
+        } catch (Exception e) {
+            System.out.println("There was an error loading the location hour data. Please check to see that is formatted correctly");
         }
     }
 
-    public void addLocationHours(String locationData){
+    private void addLocationHours(String singleLocationData){
         // takes a line in the general form of the file and adds it to the HashMap
+        String name;
+        HashMap<String, Hours> hoursByDay;
+        name = "";
+        hoursByDay = new HashMap<String, Hours>();
+        locationHoursByDay.put(name, hoursByDay);
+    }
+
+    public void getHours(String location, String day){
+        System.out.println("Opens at " + locationHoursByDay.get(location).get(day).getFirstOpeningTime().toString() + " and closes at " + locationHoursByDay.get(location).get(day).getLatestCloseTime().toString());
+    }
+
+
+    public static void main(String[] args) {
+        // tester
+        LocationHours lh = new LocationHours(new File("Data/LocationHoursData"));
     }
 }
